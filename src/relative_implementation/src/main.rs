9d987035a0d35908bc81ab4d8e4e347bc4df1cce
serhii023 <serhii023@gmail.com>
2025-03-05 17:07:44 +0200

    implement decryption

--- src/relative_implementation/src/main.rs
+++ src/relative_implementation/src/main.rs
@@ -1,7 +1,30 @@
 extern crate relative_implementation;
 
-use relative_implementation::ibbe_del7;
+use relative_implementation::ibbe_del7::IBBEDel7;
+
+fn example() {
+    let number_of_users = 15u32;
+    let (msk, pk) = IBBEDel7::run_setup(number_of_users);
+    println!("msk = {:?}\n", msk);
+    println!("pk = {:?}\n", pk);
+
+    let user_id = 8u32;
+    let sk_id = IBBEDel7::extract(&msk, user_id);
+    println!("sk_id = {:?}\n", sk_id);
+
+    let users_set = vec![1, 8, 4, 5, 3];
+
+    let (hdr, key) = IBBEDel7::encrypt(&users_set, &pk);
+    println!("c1 = {}\n", hdr.0);
+    println!("c2 = {}\n", hdr.1);
+    println!("key = {key}\n");
+
+    let decrypted_key = IBBEDel7::decrypt(&users_set, user_id, &sk_id, &hdr, &pk);
+    println!("decrypted_key = {decrypted_key}\n");
+
+    println!("key == decrypted_key: {}\n", key.eq(&decrypted_key));
+}
 
 fn main() {
-    let ibbe = ibbe_del7::IBBEDel7{};
+    example();
 }
