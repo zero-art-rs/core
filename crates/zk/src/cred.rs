67e27b7cf89ac2e0f4edde7f2e617b3b3d48b744
Yevhen Hrubiian <grubian.euhen@gmail.com>
2025-06-26 07:17:32 +0300

    add some cred functions

--- crates/zk/src/cred.rs
+++ crates/zk/src/cred.rs
@@ -1,7 +1,7 @@
 #![allow(non_snake_case)]
 use std::ops::{Add, Mul};
 use std::sync::{mpsc, Arc, Mutex};
-use std::time::{self, Instant};
+use std::time::{self, Instant, SystemTime};
 
 use rand_core::{le, OsRng};
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Valid};
@@ -16,10 +16,14 @@ use tracing::{debug, info, instrument};
 use ark_ec::{short_weierstrass::SWCurveConfig, AffineRepr, CurveGroup};
 use ark_ff::{BigInt, BigInteger, Field, PrimeField, UniformRand};
 use tracing_subscriber::field::debug;
+use zkp::toolbox::{FromBytes, SchnorrCS, ToBytes};
+use zkp::BatchableProof;
 use crate::art::R1CSProof;
 use crate::curve::cortado::{self, CortadoAffine, Parameters, ToScalar};
 use crate::poseidon::r1cs_utils::AllocatedScalar;
 use hex::FromHex;
+use zkp::toolbox::prover::Prover;
+use zkp::toolbox::verifier::Verifier;
 
 pub struct AllocatedPoint {
     pub x: AllocatedScalar,
@@ -30,8 +34,11 @@ pub struct AllocatedPoint {
 pub struct CredentialClaims {
     id: cortado::Fr, // credential unique identifier (seed)
     Q: CortadoAffine, // credential holder public key
-    expiration: u64, // credential expiration time
+    expiration: u64, // credential expiration time as UNIX timestamp
 }
+
+impl ToBytes for CredentialClaims {}
+impl FromBytes for CredentialClaims {}
 
 #[derive(Clone, Debug, CanonicalSerialize, CanonicalDeserialize)]
 pub struct Credential {
@@ -40,14 +47,55 @@ pub struct Credential {
 }
 
 impl Credential {
+    fn dl_statement<CS: SchnorrCS>(
+        cs: &mut CS,
+        x: CS::ScalarVar,
+        A: CS::PointVar,
+        B: CS::PointVar,
+    ) {
+        cs.constrain(A, vec![(x, B)]);
+    }
+
     /// issue new credential
-    pub fn issue(issuer: cortado::Fr, expiration: Instant, holder: CortadoAffine) -> Self {
-        todo!()
+    pub fn issue(issuer: cortado::Fr, expiration: SystemTime, holder: CortadoAffine) -> Result<Self, R1CSError> {
+        let claims = CredentialClaims {
+            id: issuer,
+            Q: holder,
+            expiration: expiration
+                .duration_since(time::UNIX_EPOCH)
+                .expect("expiration must be after UNIX_EPOCH")
+                .as_secs(),
+        };
+        let claims_bytes = claims.to_bytes().map_err(|_| R1CSError::FormatError)?;
+        let mut transcript = Transcript::new(b"credential_issuance");
+        transcript.append_message(b"credential", &claims_bytes);
+        let mut prover = Prover::new(b"credential_issuance", transcript);
+        let s_var = prover.allocate_scalar(b"s", issuer);
+        let (G_var, _) = prover.allocate_point(b"G", cortado::CortadoAffine::generator());
+        let Q = (cortado::CortadoAffine::generator() * issuer).into_affine();
+        let (Q_var, _) = prover.allocate_point(b"Q", Q);
+        Self::dl_statement(&mut prover, s_var, Q_var, G_var);
+        let proof = prover.prove_batchable();
+        Ok(
+            Credential { claims, signature: (proof.commitments[0], proof.responses[0]) }
+        )
     }
 
     /// verify loaded credential (only for holder)
     pub fn verify(&self) -> Result<(), R1CSError> {
-        todo!()
+        let claims_bytes = self.claims.to_bytes().map_err(|_| R1CSError::FormatError)?;
+        let mut transcript = Transcript::new(b"credential_issuance");
+        transcript.append_message(b"credential", &claims_bytes);
+        let mut verifier = Verifier::new(b"credential_issuance", transcript);
+        let s_var = verifier.allocate_scalar(b"s");
+        let G_var = verifier.allocate_point(b"G", cortado::CortadoAffine::generator()).map_err(|_| R1CSError::VerificationError)?;
+        let Q_var = verifier.allocate_point(b"Q", self.claims.Q).map_err(|_| R1CSError::VerificationError)?;
+        Self::dl_statement(&mut verifier, s_var, Q_var, G_var);
+
+        verifier.verify_batchable(&BatchableProof{
+            commitments: vec![self.signature.0],
+            responses: vec![self.signature.1],
+        }).map_err(|_| R1CSError::VerificationError)
     }
 
     /// create credential presentation proof
