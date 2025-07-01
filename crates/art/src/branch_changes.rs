8738101cd5584a382eb3449894f168ddd2566bb8
serhii023 <serhii023@gmail.com>
2025-07-01 18:10:03 +0300

    update BranchChanges

--- crates/art/src/branch_changes.rs
+++ crates/art/src/branch_changes.rs
@@ -2,6 +2,7 @@ use crate::{ARTNode, Direction, ark_de, ark_se};
 use ark_ec::AffineRepr;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use serde::{Deserialize, Serialize};
+use postcard::{from_bytes, to_allocvec};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(bound = "")]
@@ -23,4 +24,14 @@ pub struct BranchChanges<G: AffineRepr + CanonicalSerialize + CanonicalDeseriali
     #[serde(serialize_with = "ark_se", deserialize_with = "ark_de")]
     pub public_keys: Vec<G>,
     pub next: Vec<Direction>,
+}
+
+impl<G: AffineRepr + CanonicalSerialize + CanonicalDeserialize> BranchChanges<G> {
+    pub fn serialize(&self) -> postcard::Result<Vec<u8>> {
+        to_allocvec(self)
+    }
+
+    pub fn deserialize(bytes: &Vec<u8>) -> postcard::Result<Self> {
+        from_bytes(bytes)
+    }
 }
